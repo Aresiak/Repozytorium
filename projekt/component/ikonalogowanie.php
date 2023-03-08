47a36d6016206fbb164490d7d1f50409abd86e33
Aresiak <76845311+Aresiak@users.noreply.github.com>
2023-03-08 22:49:26 +0100

    Delete projekt directory

--- projekt/component/ikonalogowanie.php
+++
@@ -1,5 +0,0 @@
-<div class="avatar-icon">
-  <a href="loginpanel.php">
-    <img class="avatar-icon-img" src="https://www.salvet.pl/wp-content/uploads/2022/06/Dummy-Person-182x182-1.png" alt="Bogna" class="avatar"width="0" height="0">
-  </a>
-</div>
